e512813c31bce9d95f199b890f1a875486e1ee0d
Frcomparan <frcomparan@gmail.com>
2023-03-18 19:24:16 -0600

    Create validator for data

---
+++ middlewares/validator.handler.js
@@ -0,0 +1,18 @@
+const boom = require('@hapi/boom');
+
+function validatorHandler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    // req.body -> para POST
+    // req.params -> para GET
+    // req.query -> para GET
+
+    const { error } = schema.validate(data);
+    if (error) {
+      next(boom.badRequest(error));
+    }
+    next();
+  };
+}
+
+module.exports = validatorHandler;
