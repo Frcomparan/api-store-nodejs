ab4c26bc5ce2545ac7c68460200fedb09def80f3
Frcomparan <frcomparan@gmail.com>
2023-03-19 18:18:21 -0600

    Add relation between custemer and user models

--- api/schemas/customer.schema.js
+++ api/schemas/customer.schema.js
@@ -4,7 +4,11 @@ const id = Joi.number().integer();
 const name = Joi.string().min(3).max(30);
 const lastName = Joi.string();
 const phone = Joi.string();
+const email = Joi.string().email();
+const password = Joi.string();
 const userId = Joi.number().integer();
+
+const { createUserSchema } = require('./user.schema');
 
 const getCustomerSchema = Joi.object({
   id: id.required(),
@@ -14,7 +18,7 @@ const createCustomerSchema = Joi.object({
   name: name.required(),
   lastName: lastName.required(),
   phone: phone.required(),
-  userId: userId.required(),
+  user: createUserSchema,
 });
 
 const updateCustomerSchema = Joi.object({
